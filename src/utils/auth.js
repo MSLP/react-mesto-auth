b878bf92d423ca772d43df52b5022789c1d0fa6e
Maria Slp <mariiasmagina@gmail.com>
2021-06-22 00:34:09 +0100

    add auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,55 @@
+class Auth {
+  constructor(options) {
+    this.baseUrl = options.baseUrl;
+    this.headers = options.headers;
+  }
+
+  // проверка ответа от сервера
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    else {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+  }
+
+  // отправка данных для регистрации
+  signup(data) {
+    return fetch(`${this.baseUrl}/signup`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify(data)
+    })
+    .then(this._checkResponse);
+  }
+
+  // отправка данных для того, чтобы войти
+  signin(data) {
+    return fetch(`${this.baseUrl}/signin`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify(data)
+    })
+    .then(this._checkResponse);
+  }
+
+  // проверка наличия такого токена
+  checkToken(token) {
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization" : `Bearer ${token}`
+      }
+    })
+    .then(this._checkResponse);
+  }
+}
+
+export default new Auth({
+  baseUrl: 'https://auth.nomoreparties.co',
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
