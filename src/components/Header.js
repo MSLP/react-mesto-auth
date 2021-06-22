fa6afc3e99bbc65884b367183351b849d47250c8
Maria Slp <mariiasmagina@gmail.com>
2021-06-23 00:07:07 +0100

    add button in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import logo from '../images/Vector.svg';
 
 // шапка сайта
 function Header() {
+  const history = useHistory();
+  const location = useLocation();
+  const [buttonText, setButtonText] = React.useState('');
+
+  function handleClick() {
+    if (location.pathname === '/signup' || location.pathname === '/main') {
+      history.push('/signin')
+    }
+    else if (location.pathname === '/signin') {
+      history.push('/signup')
+    }
+  }
+
+  React.useEffect(() => {
+    if (location.pathname === '/signup') {
+      setButtonText('Войти');
+    }
+    else if (location.pathname === '/signin') {
+      setButtonText('Регистрация');
+    }
+    else if (location.pathname === '/main') {
+      setButtonText('Выйти');
+    }
+  }, [location]);
+  console.log(location);
   return (
     <header className="header">
         <img className="header__logo" src={logo} alt="логотип"/>
+        <button onClick={handleClick} className="button header__button">{buttonText}</button>
     </header>
   );
 }
